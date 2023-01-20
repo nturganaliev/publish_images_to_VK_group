92db81417f3d5f0b2a5f972ee34f622bbc38c00a
Nurlan Turganaliev <nurlan.turganaliev@gmail.com>
2023-01-20 13:10:51 +0600

    Delete fetch_xkcd_images.py Add image downloading and posting funtionality to publish_images_to_VK_group.py

--- publish_images_to_VK_group.py
+++ publish_images_to_VK_group.py
@@ -1,72 +1,138 @@
 import os
+import random
 import requests
+import time
+
+from download_image import download_image
+from get_file_extension_from_url import get_file_extension_from_url
 
 from dotenv import load_dotenv
 
-import fetch_xkcd_images
+
+load_dotenv()
+
+TOKEN = os.getenv('VK_ACCESS_TOKEN')
+GROUP_ID = os.getenv('VK_GROUP_ID')
+API_VERSION = os.getenv('VK_API_VERSION')       
 
 
-def main():
-    load_dotenv()
-
-    image_name = fetch_xkcd_images
-    token = os.getenv('VK_ACCESS_TOKEN')
-    group_id = os.getenv('VK_GROUP_ID')
-    api_version = os.getenv('VK_API_VERSION')
-    methods = ['photos.getWallUploadServer',
-               'photos.saveWallPhoto',
-               'wall.post']
-    url = f"https://api.vk.com/method/"
+def get_vk_upload_server_url():
+    url = "https://api.vk.com/method/photos.getWallUploadServer"
     params = {
-        "access_token": token,
-        "group_id": group_id,
-        "v": api_version,
+        "access_token": TOKEN,
+        "group_id": GROUP_ID,
+        "v": API_VERSION,
     }
+    response = requests.get(url, params)
+    response.raise_for_status()
+    return response.json()['response']['upload_url']
 
 
-    server_response = requests.get(f"{url}{methods[0]}", params)
-    server_response.raise_for_status()
-    upload_url = server_response.json()['response']['upload_url']
-    with open('reverse_identity_theft.png', 'rb') as file:
+def upload_photo_to_vk_server(upload_url, photo):
+    with open(photo, 'rb') as file:
         image = {
             "photo": file,
             "content-type": "multipart/form-data"
         }
-        upload_response = requests.post(upload_url, files=image)
+        response = requests.post(upload_url, files=image)
+    response.raise_for_status()
+    return response
+
+
+def save_vk_wall_photo(server, photo, vk_hash):
+    url = "https://api.vk.com/method/photos.saveWallPhoto"
+    params = {
+        "access_token": TOKEN,
+        "group_id": GROUP_ID,
+        "v": API_VERSION,
+        'server': server,
+        'photo': photo,
+        'hash': vk_hash,
+    }
+    response = requests.post(url, params)
+    response.raise_for_status()
+    return response
+
+
+def get_vk_wall_photo_id(response):
+    response_content = response.json()
+    owner_id = response_content['response'][0]['owner_id']
+    media_id = response_content['response'][0]['id']
+    return f"photo{owner_id}_{media_id}"
+
+
+def post_photo_to_vk_group(photo_id, text):
+    url = "https://api.vk.com/method/wall.post"
+    params = {
+        "access_token": TOKEN,
+        "attachments": photo_id,
+        "message": text,
+        "owner_id": f"-{GROUP_ID}",
+        "from_group": 0,
+        "v": API_VERSION,
+    }
+    response = requests.post(url, params)
+    response.raise_for_status()
+    return response.json()
+
+
+def get_random_number_for_xkcd():
+    url = "https://xkcd.com/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    last_image_number = response.json()["num"]
+    return random.randint(1, last_image_number)
+
+
+def main():
+    try:
+        random_number = get_random_number_for_xkcd()
+    except requests.exceptions.RequestException as error:
+        print(error)
+    
+    url = f"https://xkcd.com/{random_number}/info.0.json"
+    response = requests.get(url)
+    response.raise_for_status()
+    response_content = response.json()
+    image_url = response_content["img"]
+    image_name = response_content["title"].lower()
+    image_extension = get_file_extension_from_url(image_url)
+    image_text = response_content['alt']
+    path = os.path.join(os.path.abspath("."), f"{image_name}{image_extension}")
+    
+    try:
+        download_image(image_url, path)
+        upload_url = get_vk_upload_server_url()
+        upload_response = upload_photo_to_vk_server(upload_url, path)
+    except requests.exceptions.RequestException as error:
+        print(error)
 
     upload_response_content = upload_response.json()
-    params = {
-        "access_token": token,
-        'server': upload_response_content['server'],
-        'photo': upload_response_content['photo'],
-        'hash': upload_response_content['hash'],
-        "group_id": group_id,
-        "v": api_version,
-    }
+    server = upload_response_content['server'],
+    photo = upload_response_content['photo'],
+    vk_hash = upload_response_content['hash'],
 
-    save_response = requests.post(f"{url}{methods[1]}", params)
-    save_response_content = save_response.json()
-    image_url = save_response_content['response'][0]['sizes'][2]['url']
-    owner_id = save_response_content['response'][0]['owner_id']
-    media_id = save_response_content['response'][0]['id']
-    print()
-    print(image_url)
-    with open('reverse_identity_theft.txt', 'r') as f:
-        comment = f.read()
+    try:
+        save_response = save_vk_wall_photo(server, photo, vk_hash)
+    except requests.exceptions.RequestException as error:
+        print(error)
 
-    photo_id = f"photo{owner_id}_{media_id},{image_url}"
-    params = {
-        "access_token": token,
-        "attachements": image_url,
-        "message": comment,
-        "owner_id": f"-{group_id}",
-        "from_group": 0,
-        "v": api_version,
-    }
+    photo_id = get_vk_wall_photo_id(save_response)
 
-    post_url = f"https://api.vk.com/method/{methods[2]}"
-    post_response = requests.post(post_url, params)
-    print(post_response.json())
+    try:
+        post_photo_to_vk_group(photo_id, image_text)
+    except requests.exceptions.RequestException as error:
+        print(error)
+
+    time.sleep(10)
+
+    if not os.path.exists(path):
+        print("Файл не существует.")
+        print("Завершение работы.")
+        return
+    os.remove(path)
+    print("Завершение работы.")
+
 
 if __name__ == '__main__':
     main()
